41b774ae9242d14b944cae602409cee8547dd2b9
George Giagkos <george.giagkos91@gmail.com>
2023-01-18 21:54:27 +0200

    Initial Commit

---
+++ src/components/CellList.tsx
@@ -0,0 +1,34 @@
+import './cellList.css';
+import { Fragment, useEffect } from 'react';
+import { useTypedSector } from '../hooks/useTypedSelector';
+import AddCell from './AddCell';
+import CellListItem from './CellListItem';
+import { useActions } from '../hooks/useActions';
+
+const CellList: React.FC = () => {
+	const cells = useTypedSector(({ cells: { order, data } }) =>
+		order.map((id) => data[id])
+	);
+
+	const { fetchCells } = useActions();
+
+	useEffect(() => {
+		fetchCells();
+	}, []);
+
+	const renderedCells = cells.map((cell) => (
+		<Fragment key={cell.id}>
+			<CellListItem cell={cell} />
+			<AddCell previousCellId={cell.id} />
+		</Fragment>
+	));
+
+	return (
+		<div className="cell-list">
+			<AddCell forceVisible={cells.length === 0} previousCellId={null} />
+			{renderedCells}
+		</div>
+	);
+};
+
+export default CellList;
